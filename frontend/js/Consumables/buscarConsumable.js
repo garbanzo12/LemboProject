f920aad37eab9102cb45f490eeb605f8507a3c41
garbanzo12 <122324891+garbanzo12@users.noreply.github.com>
2025-05-05 09:43:26 -0500

    Modulo de insumos ✅
    El modulo de insumos ya tiene buscar, visualizar, actualizar y listar

--- frontend/js/Consumables/buscarConsumable.js
+++ frontend/js/Consumables/buscarConsumable.js
@@ -1,11 +1,11 @@
 console.log('Script cargado');
 
-async function obtenerIdsCiclo() {
+async function obtenerIdsInsumo() {
   try {
-    const res = await fetch('http://localhost:5501/cropcycle/id'); // ✅ Nuevo endpoint
-    if (!res.ok) throw new Error('No se pudieron obtener los Ciclos');
+    const res = await fetch('http://localhost:5501/consumables/id'); // ✅ Nuevo endpoint
+    if (!res.ok) throw new Error('No se pudieron obtener los Insumos');
     const data = await res.json();
-    return data.ciclos; // ✅ Ya viene como un array de IDs
+    return data.insumos; // ✅ Ya viene como un array de IDs
   } catch (err) {
     console.error('Error al obtener los IDs:', err.message);
     return [];
@@ -16,12 +16,12 @@ async function inicializarBuscar() {
   const formBuscar = document.querySelector('.cardright__form');
   const selectId = document.querySelector('.cardright__selectid');
 
-  const ids = await obtenerIdsCiclo();
+  const ids = await obtenerIdsInsumo();
   selectId.innerHTML = '';
 
   if (ids.length === 0) {
     const option = document.createElement('option');
-    option.textContent = 'No hay Ciclo disponibles';
+    option.textContent = 'No hay Insumo disponibles';
     option.disabled = true;
     selectId.appendChild(option);
   } else {
@@ -55,12 +55,12 @@ async function inicializarBuscar() {
     }
 
     try {
-      const res = await fetch(`http://localhost:5501/api/cropcycle/${id}`);
+      const res = await fetch(`http://localhost:5501/api/consumables/${id}`);
       if (!res.ok) throw new Error('No se encontró el cultivo');
       const data = await res.json();
 
-      localStorage.setItem('Cicloseleccionado', JSON.stringify(data));
-      window.location.href = '3-view_cycle_crops.html';
+      localStorage.setItem('Insumoseleccionado', JSON.stringify(data));
+      window.location.href = '3-view-insumes.html';
     } catch (err) {
       alert('Error: ' + err.message);
     }
